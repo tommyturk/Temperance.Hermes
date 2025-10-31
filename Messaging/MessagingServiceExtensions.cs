4961583878a4841bf001e1501c96319e09a6ab31
Tommy Memed <tommymemed@gmail.com>
2025-10-31 08:21:35 +0000

    feat: Add MessagingServiceExtensions and bump version to 1.0.0

---
+++ Messaging/MessagingServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Temperance.Hermes.Configuration;
+using Temperance.Hermes.Connection;
+using Temperance.Hermes.Publishing;
+
+namespace Temperance.Hermes.Messaging
+{
+    public static class MessagingServiceExtensions
+    {
+        public static IServiceCollection AddTemperanceMessaging(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<RabbitMqSettings>(configuration.GetSection("RabbitMqSettings"));
+            services.AddSingleton<IRabbitMqConnectionFactory, RabbitMqConnectionFactory>();
+            services.AddSingleton<IMessagePublisher, RabbitMqPublisher>();
+            return services;
+        }
+    }
+}
